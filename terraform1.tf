d82420e3af1db36697c8fe32c71c484e75cc22d2
lavudishashikala <164206889+lavudishashikala@users.noreply.github.com>
2024-07-16 18:37:11 +0530

    Update terraform1.tf

--- terraform1.tf
+++ terraform1.tf
@@ -3,143 +3,130 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
 
-# Configure the AWS provider
+# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
 }
+
 
 # Create a VPC
 resource "aws_vpc" "proj-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Create an internet gateway
+#Create a Gateway
 resource "aws_internet_gateway" "proj-ig" {
   vpc_id = aws_vpc.proj-vpc.id
+
   tags = {
     Name = "gateway1"
   }
 }
 
-# Set up the route table
+# Setting up the routing table
 resource "aws_route_table" "proj-rt" {
   vpc_id = aws_vpc.proj-vpc.id
-  route {
+
+  route { 
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.proj-ig.id
   }
+
   route {
-    ipv6_cidr_block = "::/0"
+    ipv6_cidr_block        = "::/0"
     gateway_id = aws_internet_gateway.proj-ig.id
   }
+
   tags = {
     Name = "rt1"
   }
 }
 
-# Set up the subnet
+
+#Create Subnet
 resource "aws_subnet" "proj-subnet" {
-  vpc_id            = aws_vpc.proj-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  vpc_id     = aws_vpc.proj-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone= "us-east-1a"
   tags = {
     Name = "subnet1"
   }
 }
 
-# Associate the subnet with the route table
+#associate the subnet with the route table
 resource "aws_route_table_association" "proj-rt-sub-assoc" {
-  subnet_id      = aws_subnet.proj-subnet.id
-  route_table_id = aws_route_table.proj-rt.id
+        subnet_id = aws_subnet.proj-subnet.id
+        route_table_id = aws_route_table.proj-rt.id
 }
 
-# Create a security group
+
+
+
+# Create Scurity Groups
 resource "aws_security_group" "proj-sg" {
   name        = "proj-sg"
-  description = "Enable web traffic for the project"
+  description = "enable traffic on port 22,80 and 443"
   vpc_id      = aws_vpc.proj-vpc.id
+
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "All Traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
+
+
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
-  ingress {
-    description = "HTTPS traffic"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "Allow port 80 inbound"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   tags = {
     Name = "proj-sg1"
   }
 }
 
-# Create a key pair
-resource "aws_key_pair" "jenkins" {
-  key_name   = "jenkins"
-  public_key = file("~/.ssh/jenkins.pem")
-}
 
-# Create a new network interface
+
+
+#Create Network interfaces
 resource "aws_network_interface" "proj-ni" {
   subnet_id       = aws_subnet.proj-subnet.id
+  private_ips     = ["10.0.1.25"]
   security_groups = [aws_security_group.proj-sg.id]
-  private_ips     = ["10.0.1.10"]
 }
 
-# Attach the elastic IP to the network interface
+#Creating Elastic IP
 resource "aws_eip" "proj-eip" {
-  vpc = true
-  network_interface = aws_network_interface.proj-ni.id
-  depends_on        = [aws_network_interface.proj-ni]
-}
-# Create a TLS private key for Jenkins
-resource "tls_private_key" "jenkins" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+  vpc                       = true
+  network_interface         = aws_network_interface.proj-ni.id
+  associate_with_private_ip = "10.0.1.25"
 }
 
-# Create an AWS key pair for Jenkins
-resource "aws_key_pair" "jenkins" {
-  key_name   = "jenkins"
-  public_key = tls_private_key.jenkins.public_key_openssh
-}
 
-# Create an Ubuntu EC2 instance
-resource "aws_instance" "Prod-Server" {
-  ami               = "ami-0c2af51e265bd5e0e"
-  instance_type     = "t2.medium"
-  availability_zone = "ap-south-1a"
-  key_name          = aws_key_pair.jenkins.key_name
+#Creating Ec2 instance
+resource "aws_instance" "proj-instance" {
+  ami           = "ami-0c2af51e265bd5e0e"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "jenkins"
 
   network_interface {
-    device_index          = 0
-    network_interface_id  = aws_network_interface.proj-ni.id
+    network_interface_id = aws_network_interface.proj-ni.id
+    device_index         = 0
   }
-}
+  
 
-  user_data = <<EOF
-#!/bin/bash
-sudo apt-get update -y
-EOF
+  tags = {
+      Name = "prod-server"
+  }
 }
