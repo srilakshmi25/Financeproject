8f527a783f2eab3dc0f45078188d671983d5593e
faizan32419 <130635112+faizan32419@users.noreply.github.com>
2024-09-11 15:51:56 +0530

    Updated main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,32 @@
-resource "aws_instance" "test-server"{
-  ami = "ami-0e86e20dae9224db8"
+resource "aws_instance" "test-server" {
+  ami           = "ami-0e86e20dae9224db8"  # Replace with your AMI
   instance_type = "t2.micro"
-  key_name = "ansiblef"
-  vpc_security_group_ids =["sg-078c941ec791c7003"]
-  connection {
-     type = "ssh"
-     user = "ubuntu"
-     private_key = file("./ansiblef.pem")
-     host = self.public_ip
-     }
-  provisioner "remote-exec" {
-     inline = ["echo wait to start the instance' "]
-  }
+  key_name      = "ansiblef"
+  vpc_security_group_ids = ["sg-078c941ec791c7003"]
+
   tags = {
-     Name = "test-server"
-     }
+    Name = "test-server"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./ansiblef.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'wait to start the instance'"
+    ]
+  }
+
   provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
+    command = "echo ${self.public_ip} > inventory"
+  }
+
   provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/Care-Health/terraform-files/ansibleplaybook.yml"
-     }
-     }
+    command = "ansible-playbook /var/lib/jenkins/workspace/Care-Health/terraform-files/ansibleplaybook.yml"
+  }
+}
+
